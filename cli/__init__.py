aad014de12f44d4f7cb4c9a78dce5aa9a58ffa5f
Quinten <quinten.roets@gmail.com>
2022-01-16 23:59:25 +0100

    use rich in import

--- cli/__init__.py
+++ cli/__init__.py
@@ -3,3 +3,9 @@ from .input_interface import ask
 from .install import install, get_install_command
 from .output_interface import Message as message, Spinner as spinner
 from .run import get, lines, run, run_commands, sh, start, urlopen
+
+from rich.console import Console
+
+console = Console()
+
+status = console.status
