3b67916c29248c6df5ba1947210c2603ac9a91f1
Quinten <quinten.roets@gmail.com>
2022-02-20 21:41:54 +0100

    make progress bar wider

--- cli/progress.py
+++ cli/progress.py
@@ -1,13 +1,21 @@
-def progress(sequence, description=None, total=None, unit='it', description_width=None):
-    from rich.progress import TextColumn, BarColumn, TimeRemainingColumn, Progress  # lazy imports
+def progress(sequence, description=None, total=None, unit="it", description_width=None):
+    from rich.progress import (
+        TextColumn,
+        BarColumn,
+        TimeRemainingColumn,
+        Progress,
+    )  # lazy imports
 
     columns = (
         [TextColumn("[progress.description]{task.description}")] if description else []
     )
     columns.extend(
         (
-            TextColumn("[progress.completed]{task.completed}/[progress.total]{task.total:>0.0f} " + unit),
-            BarColumn(bar_width=500), # shrinks depending on other columns
+            TextColumn(
+                "[progress.completed]{task.completed}/[progress.total]{task.total:>0.0f} "
+                + unit
+            ),
+            BarColumn(bar_width=1000),  # shrinks depending on other columns
             TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
             TimeRemainingColumn(),
         )
@@ -15,6 +23,4 @@ def progress(sequence, description=None, total=None, unit='it', description_widt
     progress = Progress(*columns)
 
     with progress:
-        yield from progress.track(
-            sequence, total=total, description=description
-        )
+        yield from progress.track(sequence, total=total, description=description)
