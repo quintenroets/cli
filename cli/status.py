19dd423d86cae84935bde5a8cf7eb62d5d4a59fc
Quinten <quinten.roets@gmail.com>
2022-03-26 12:38:05 +0100

    only show status if not in notebook

--- cli/status.py
+++ cli/status.py
@@ -2,8 +2,15 @@ def status(*args, **kwargs):
     import cli
     import contextlib
 
+    try:
+        get_ipython()
+    except:
+        notebook = False
+    else:
+        notebook = True
+
     return (
         cli.console.status(*args, **kwargs)
-        if cli.console._live is None
+        if cli.console._live is None and not notebook
         else contextlib.nullcontext()
     )
