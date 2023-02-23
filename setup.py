e347b273776dffdeac98a092ebce174a49c7c428
Quinten <quinten.roets@gmail.com>
2023-02-23 16:19:07 -0500

    migrate setup to setup.cfg

--- setup.py
+++ setup.py
@@ -1,28 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-NAME = "quinten-cli"
-
-
-def read(filename):
-    try:
-        with open(filename) as fp:
-            content = fp.read().split("\n")
-    except FileNotFoundError:
-        content = []
-    return content
-
-
-setup(
-    author="Quinten Roets",
-    author_email="quinten.roets@gmail.com",
-    description="extended subprocess",
-    name=NAME,
-    version="1.0.1",
-    packages=find_packages(),
-    install_requires=read("requirements.txt"),
-    entry_points={
-        "console_scripts": [
-            "konsolerun = cli.run:main",
-        ]
-    },
-)
+if __name__ == "__main__":
+    setup()
