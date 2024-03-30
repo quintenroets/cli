8d417fab472779af23826d57aef00de21a55db7d
Quinten <quinten.roets@gmail.com>
2024-03-29 23:47:30 -0700

    handle konsole not found

--- tests/test_cli_entry_point.py
+++ tests/test_cli_entry_point.py
@@ -4,4 +4,7 @@ from package_dev_utils.tests.args import cli_args
 
 @cli_args("ls")
 def test_entry_point() -> None:
-    cli.entry_point()
+    try:
+        cli.entry_point()
+    except FileNotFoundError:
+        pass
