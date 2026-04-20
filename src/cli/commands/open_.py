5ee69a26fb8e33aa72bdc54ebc3e72b3810cc234
Quinten <quinten.roets@gmail.com>
2026-04-19 22:37:35 -0700

    support mac url open

--- src/cli/commands/open_.py
+++ src/cli/commands/open_.py
@@ -2,11 +2,14 @@ import os
 
 from .commands import StringLike
 from .run import launch
+import platform
 
 
 def open_urls(*urls: StringLike) -> None:
     for url in urls:
         if os.name == "nt":
             os.startfile(url)  # type: ignore[attr-defined] # noqa: S606 # pragma: nocover
-        else:
+        elif platform.system() == "Linux":
             launch("xdg-open", url)
+        else:
+            launch("open", url)
