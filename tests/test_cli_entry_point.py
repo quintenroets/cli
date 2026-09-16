c10a9118143a2b9785df1119f9be7cd01af20721
Quinten Roets <62651391+quintenroets@users.noreply.github.com>
2026-09-15 19:43:49 -0700

    merge root tests (#59)

--- tests/test_cli_entry_point.py
+++ tests/test_cli_entry_point.py
@@ -1,4 +1,3 @@
-import contextlib
 from unittest.mock import MagicMock, patch
 
 from package_dev_utils.tests.args import cli_args
@@ -9,6 +8,5 @@ from cli import cli
 @patch("subprocess.Popen")
 @cli_args("ls")
 def test_run_in_new_tab(mocked_popen: MagicMock) -> None:
-    with contextlib.suppress(FileNotFoundError):
-        cli.run_in_new_tab()
+    cli.run_in_new_tab()
     mocked_popen.assert_called_once()
