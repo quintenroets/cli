a8767ddb5ad475489d9739e5971700fa45d5421f
Quinten <quinten.roets@gmail.com>
2026-09-15 20:47:11 -0700

    remove set title

--- src/cli/output/__init__.py
+++ src/cli/output/__init__.py
@@ -1,4 +1,3 @@
-from .console import set_title
 from .progress import track_progress
 from .rich import console
 from .status import status
--- src/cli/output/console.py
+++
@@ -1,7 +0,0 @@
-from cli.commands import run
-
-
-def set_title(title: str) -> None:
-    echo_message = f"\\033]30;{title}\\007"
-    command = f'echo -ne "{echo_message}"'
-    run(command)
