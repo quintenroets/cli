d012caf35552f3a894cd61d47e90f06f9d4f7b6a
Quinten Roets <62651391+quintenroets@users.noreply.github.com>
2026-09-23 21:47:07 -0700

    remove sudo rewrite (#68)

--- src/cli/commands/commands.py
+++ src/cli/commands/commands.py
@@ -4,7 +4,6 @@ import sys
 import typing
 from collections.abc import Iterable, Iterator, Sequence
 from dataclasses import dataclass
-from functools import cached_property
 from pathlib import Path
 from typing import Protocol, cast
 
@@ -25,30 +24,17 @@ class CommandPreparer:
     new_tab: bool = False
     use_root: bool = False
     title: str | None = None
-    askpass_key: str = "SUDO_ASKPASS"
-    root_keyword: str = "sudo"
 
     def run(self) -> tuple[str, ...]:
-        use_shell_command = self.use_shell_command
-        return (
-            self.prepare_shell_command()
-            if use_shell_command
-            else self.prepare_command()
+        parts = (
+            self.generate_shell_command_parts()
+            if self.use_shell_command
+            else self.generate_command_parts()
         )
-
-    def prepare_command(self) -> tuple[str, ...]:
-        return tuple(self.generate_command_parts())
-
-    def prepare_shell_command(self) -> tuple[str, ...]:
-        return tuple(self.generate_shell_command_parts())
+        return tuple(parts)
 
     def generate_shell_command_parts(self) -> Iterator[str]:
         command = " ".join(self.generate_command_parts())
-        if self.should_use_root(command):
-            if not command.startswith(self.root_keyword):
-                command = f"{self.root_keyword} {command}"
-            if self.askpass_is_available:
-                command = command.replace(self.root_keyword, f"{self.root_keyword} -A")
         if self.new_tab:
             yield from self.generate_new_tab_command_parts(command)
         else:
@@ -69,37 +55,9 @@ class CommandPreparer:
                 command = f"echo -ne '\\033]30;{self.title}\\007'; " + command
             yield command
 
-    @cached_property
-    def askpass_is_available(self) -> bool:
-        askpass_program = os.environ.get(self.askpass_key)
-        return askpass_program is not None and Path(askpass_program).exists()
-
-    def should_use_root(self, first_command_part: str) -> bool:
-        if os.name == "posix":
-            should_use_root = self.use_root or self.root_keyword in first_command_part
-        else:
-            should_use_root = False  # pragma: nocover
-        return should_use_root
-
     def generate_command_parts(self) -> Iterator[str]:
-        command_parts = self._generate_command_parts()
-        first_part = next(command_parts, None)
-        if first_part is not None:
-            if self.use_shell_command:
-                yield first_part
-            else:
-                yield from self.generate_root_parts(first_part)
-        yield from command_parts
-
-    def generate_root_parts(self, first_part: str) -> Iterator[str]:
-        if self.should_use_root(first_part):
-            yield self.root_keyword
-            if self.askpass_is_available:
-                yield "-A"
-        if first_part != self.root_keyword:
-            yield first_part
-
-    def _generate_command_parts(self) -> Iterator[str]:
+        if self.use_root and os.name == "posix":
+            yield "sudo"
         for i, item in enumerate(self.items):
             if i == 0 and isinstance(item, str) and not self.use_shell_command:
                 # allow first argument in the form of a command
